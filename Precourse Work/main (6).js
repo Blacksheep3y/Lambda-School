1f96eca203538c860eb8fa8e27e126f6b08c2b5e
Matthew <Blacksheep3y@gmail.com>
2019-12-06 16:24:11 -0500

    Add files via upload

---
+++ Precourse Work/main (6).js
@@ -0,0 +1,26 @@
+// Exercise One: 
+// Step One: Create a function called 'parametersExercise'. 
+// Step Two: This function will need to take two parameters, call them 'param1' and 'param2'
+// Step Three: The function should console log the second parameter.
+function parametersExercise(param1, param2){
+  console.log(param2);
+}
+
+// DO NOT CHANGE ANYTHING ON THE NEXT FOUR LINES!
+function exerciseTwo(){
+  function sayMyName(parameter1){
+    console.log(parameter1)
+  }
+  
+
+// Continue below this line:
+
+// Exercise Two:
+// Step One create a variable called 'myName' and assign it the a string of your name.
+// Step Two: Call the function called "sayMyName",passing the 'myName' variable as it's only argument.
+// NOTE: You do NOT need to create the function (sayMyName), doing so will break the test. 
+//       It has been created for you.
+var myName = "Matthew";
+sayMyName(myName);
+
+}
